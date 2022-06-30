1e05b3ad0d7fc78256c912c432788de72bc4923e
isagomz <isaiasgomz027@gmail.com>
2022-06-30 13:21:56 -0500

    homework 03

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,51 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http.createServer((req, res) =>{
+  if(req.url === '/api'){
+    res.writeHead(200, {'Content-type':'application/json'})
+    res.end(JSON.stringify(beatles))
+  }
+
+
+
+  if(req.url.substring(0,5) === '/api/' && req.url.length > 5){
+
+    let beatle = req.url.split('/').pop()
+    let foundBeatle = beatles.find(b => beatle === encodeURI(b.name))
+
+    if(foundBeatle){
+      res.writeHead(200,{'Content-type':'application/json'})
+      res.end(JSON.stringify(foundBeatle))
+    }else{
+      res.writeHead(200,{'Consten-type':'text-plain'})
+      res.end('Lo sentimos el Beatle no se encontro')
+    }
+
+  }
+
+  if(req.url === '/'){
+    res.writeHead(200,{'Content-type':'text/html'})
+    let file = fs.readFileSync(`${__dirname}/index.html`) 
+    res.end(file)
+  }
+
+  let beatle = req.url.split('/').pop()
+  let search = beatles.find((b) => beatle  === encodeURI(b.name))
+  if(search){
+    res.writeHead(200,{'Content-type':'text/html'})
+    let file = fs.readFileSync(`${__dirname}/beatle.html`,'utf-8')
+    file = file.replace(/{names}/g, search.name)
+    file = file.replace('{profilePic}',search.profilePic)
+    file = file.replace('{birthdate}', search.birthdate)
+    res.end(file)
+
+  }else{
+    res.writeHead(404,{'Content-type':'text/plain'})
+    res.end('Lo sentimos no se encontro el beatle')
+  }
+  
+  
+
+}).listen(3000, '127.0.0.1')
