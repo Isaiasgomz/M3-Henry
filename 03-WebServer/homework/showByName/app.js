448618cd9f0bd4fec70f63762cbb608b66a83dc2
isagomz <isaiasgomz027@gmail.com>
2022-07-01 09:56:05 -0500

    homework 04-p2

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,18 @@
 var fs  = require("fs")
 var http  = require("http")
 
+
 // Escribí acá tu servidor
+http.createServer(function(req, res){
+    // res.url
+    fs.readFile(`${__dirname}/images/${req.url}.jpg`, function(err,data){
+        if(err){
+            res.writeHead(404, {'Content-type':'text-plain'})
+            res.end('Lo sentimos perri no encontrado')
+        }else{
+            res.writeHead(200,{'Content-type':'image/jpg'})
+            res.end(data)}
+    })
+    
+    
+}).listen(3001,'127.0.0.1')
